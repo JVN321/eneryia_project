4aaefa330a209a06783de9e572f4df37650377e4
JVN <johnvnettady@gmail.com>
2025-02-13 15:26:06 +0530

    added some boiler plate content

---
+++ app/index/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const IndexPage = () => {
+return (
+    <div className="h-screen">
+        <div className="flex justify-center items-center h-full">
+
+                <div className="text-4xl text-someRed font-bold">Welcome to Eneryia Project</div>
+
+        </div>
+    </div>
+);
+};
+
+export default IndexPage;
