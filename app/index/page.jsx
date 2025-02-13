26b088611c9086e616f560d363c6677e68fa6180
JVN <johnvnettady@gmail.com>
2025-02-13 22:41:16 +0530

    test commit

--- app/index/page.jsx
+++ app/index/page.jsx
@@ -8,6 +8,8 @@ return (
         <div className="flex flex-col gap-5 justify-center items-center h-full">
 
                 <div className="text-4xl text-someRed font-bold">Welcome to Eneryia Project</div>
+                <div className="text-4xl text-someRed font-bold">KARTHICCCC HAS A THICC ASS</div>
+                <div className="text-4xl text-someRed font-bold">ABEL GAYYYYYY</div>
                 <Button variant="bordered">some random button</Button>
         </div>
     </div>
