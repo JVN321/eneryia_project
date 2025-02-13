2ac861c7ab1b38d9028aaa7f6b76e6738c1fc14e
JVN <johnvnettady@gmail.com>
2025-02-13 15:47:54 +0530

    added more boilerplate code

--- app/index/page.jsx
+++ app/index/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
+import {Button} from "@heroui/button";
 
 const IndexPage = () => {
 return (
     <div className="h-screen">
-        <div className="flex justify-center items-center h-full">
+        <div className="flex flex-col gap-5 justify-center items-center h-full">
 
                 <div className="text-4xl text-someRed font-bold">Welcome to Eneryia Project</div>
-
+                <Button variant="bordered">some random button</Button>
         </div>
     </div>
 );
